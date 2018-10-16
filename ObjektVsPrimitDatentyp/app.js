c992d5a56aa49959cd97f7c3e1db02d669af42bf
Moritz Weibold <moritz@weibold.cc>
2018-10-16 19:44:50 +0200

    Uebung von 15.10.18

---
+++ ObjektVsPrimitDatentyp/app.js
@@ -0,0 +1,12 @@
+let x = {value: 10};
+let y = x;
+x.value = 20;
+console.log("x = ", x.value);
+console.log("y = ", y);
+//Objekt
+
+let x = 10;
+let y = x;
+x = 20;
+console.log("x = ", x);
+console.log("y = ", y);
